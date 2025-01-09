b893dd02a003d49af0a58c20bcdd1517d9bf0e03
jayceforte <jayce4tay@gmail.com>
2025-01-09 10:11:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const freelancers = [
+    {
+        name: 'Alice',
+        occupation:'Writer',
+        startingPrice: 30
+    },
+    {
+        name: 'Bob',
+        occupation: 'Teacher',
+        startingPrice: 50
+    },
+]
+const possibleFreeLancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+// function that calculates average starting price
+function calculateAverageStartingPrice() {};
+
+
+// add a freelancer to the existing array
+function addFreelancer() {
+//  add a bunch of carols
+}
+// iterate through freelancers to create an element for each one, add info to the element
+function renderFreelancers (freelancers) {
+// iterates through array ....
+}
+
+// function that renders items onto the DOM
+function render() {
+
+}
+
+
+setInterval(() => {
+   addFreelancer();
+   render();
+}, 1000);
