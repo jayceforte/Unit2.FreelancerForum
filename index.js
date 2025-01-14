9543f88fe9e6e0ff024556b3c81e5e2adcea0d77
jayceforte <jayce4tay@gmail.com>
2025-01-13 19:02:18 -0700

    finished

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const freelancers = [
-    {
-        name: 'Alice',
-        occupation:'Writer',
-        startingPrice: 30
-    },
-    {
-        name: 'Bob',
-        occupation: 'Teacher',
-        startingPrice: 50
-    },
-]
-const possibleFreeLancers = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-  { name: "Prof. Prism", price: 81, occupation: "teacher" },
-  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-  { name: "Prof. Spark", price: 76, occupation: "programmer" },
-  { name: "Dr. Wire", price: 47, occupation: "teacher" },
-  { name: "Prof. Goose", price: 72, occupation: "driver" },
-];
+    { name: "Alice", occupation: "Writer", startingPrice: 30 },
+    { name: "Bob", occupation: "Teacher", startingPrice: 50 }
+  ];
+
+
+
+
+function displayFreelancers(){
+const freelancersList = document.getElementById("freelancersList")
+freelancersList.innerHTML ="";
+
+freelancers.forEach((freelancer) =>{
+    const freelancerDiv = document.createElement("div")
+      freelancerDiv.innerHTML = `<p>Name: ${freelancer.name}</p>
+                                 <p>Occupation: ${freelancer.occupation}</p>
+                                 <p>Starting Price: $${freelancer.startingPrice}</p>`;
+    freelancersList.appendChild(freelancerDiv)
+})
+};
 // function that calculates average starting price
-function calculateAverageStartingPrice() {};
-
-
+function calculateAverageStartingPrice() {
+const totalPrices = freelancers.reduce((sum, freelancer) => sum + freelancer.startingPrice, 0);
+    const averagePrice = totalPrices / freelancers.length;
+    const averagePriceSpan = document.getElementById("averagePrice");
+    averagePriceSpan.textContent = `$${averagePrice.toFixed(2)}`;
+}
 // add a freelancer to the existing array
-function addFreelancer() {
-//  add a bunch of carols
+function addnewFreelancer() {
+    const newFreelancer = { name: "Carol", occupation: "Programmer", startingPrice: 70};
+freelancers.push(newFreelancer);
+displayFreelancers();
+calculateAverageStartingPrice();
+    //  add a bunch of carols
 }
 // iterate through freelancers to create an element for each one, add info to the element
 function renderFreelancers (freelancers) {
 // iterates through array ....
+
 }
 
 // function that renders items onto the DOM
@@ -39,7 +45,7 @@ function render() {
 }
 
 
-setInterval(() => {
-   addFreelancer();
-   render();
-}, 1000);
+setInterval(addnewFreelancer, 3000);
+
+displayFreelancers();
+calculateAverageStartingPrice();
